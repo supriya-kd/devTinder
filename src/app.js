d1154b995223fe55f01d1c53614a425af4d2b437
daberaopawan <daberaopawan@hotmail.com>
2025-08-22 16:36:35 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use('/test', (req, res) => {
+    res.send('Hello World from Server');
+});
+
+app.use('/', (req, res) => {
+    res.send('Hello World from <b>Home');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
